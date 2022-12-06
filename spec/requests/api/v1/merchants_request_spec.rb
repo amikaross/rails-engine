41329a975b5270787163350081defa4d290e2148
Amanda Ross <amikaross@gmail.com>
2022-12-06 07:50:51 -0700

    Completes merchant side of part one, NOT INCLUDING the correct json formatting using serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Merchants API" do 
-  it "sends a list of merchants" do 
+  it "get all merchants" do 
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
@@ -16,5 +16,46 @@ describe "Merchants API" do
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
     end
+  end
+
+  it "can get one merchant by their id" do 
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful 
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
+
+  it "can get all items for a given merchant id" do 
+    merchant = create(:merchant)
+    item_1 = create(:item, merchant: merchant)
+    item_2 = create(:item, merchant: merchant)
+    item_3 = create(:item, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    merchant_items = JSON.parse(response.body, symbolize_names: true)
+    item = merchant_items.first
+
+    expect(response).to be_successful
+
+    expect(merchant_items.count).to eq(3)
+    
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_a(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_an(Integer)
   end
 end
